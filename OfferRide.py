743c0228ea4f9d7858180a2424e55f20050ad3e6
Alan <37944809+Starscream29@users.noreply.github.com>
2018-11-03 12:54:28 -0600

    offer - Ride functionality

--- OfferRide.py
+++ OfferRide.py
@@ -3,12 +3,27 @@ from ValidateInputs import*
 
 def OfferRide(user):
     print("========")
-    print("Offering rides:")
-    dateInput = input("Please enter the date of your ride (YYYY-MM-DD)> ")
-    Vali_Date(dateInput)
+    print("Offering rides:(mandatory fields)")
+    while True:
+        dateInput = input("Please enter the date of your ride (YYYY-MM-DD)> ")
+        if Vali_Date(dateInput) is False:
+            print("Enter date properly you fuck")
+        else:
+            break
+    seatNumber = input("Please enter the number of seat you are offering> ")
+    seatPrice = input("Please enter your asking price for each seat ($)> ")
+    luggage = input("Please enter a quick luggage description>")
+    locationSrc = getLocation(input("Enter your starting location and select from the list")) #Returns LCODE
+    locationDes = getLocation(input("Enter your destination location and select from the list")) #Returns LCODE
+
+    print("========")
+    print("Offering rides:(Optional, leave blank if not applicable)")
+
+    #TODO: get optional fields, car number and some enroute locations
 
 
 
+    #TODO: Finally, smash all these fields into an sqlite insert statement, then go back to membersoptions menu
 
 
 
--- ValidateInputs.py
+++ ValidateInputs.py
@@ -1,8 +1,11 @@
 
-#Function to validate inputs
+import sqlite3
 
+#
+#This file to mostly just hold misc functions that're gonna be called a whole bunch
+#
 
-import datetime
+from datetime import datetime
 
 #Validate the date
 def Vali_Date(date_text):
@@ -15,3 +18,27 @@ def Vali_Date(date_text):
 
 
     return True
+
+def getLocation(input):
+    input = "%" + input + "%"
+
+    conn = sqlite3.connect('./Database.db')
+    c = conn.cursor()
+    c.execute("select * from locations where city like ? or address like ? or prov like ? or lcode like ?", (input, input, input, input))
+
+    LocationResults = c.fetchall()
+    print(LocationResults)
+    a = 0
+    b = 5
+
+    for n in range(len(LocationResults[a:b])):
+        print(n + a, "  :  ", LocationResults[n + a])
+
+    print("Enter the number of your intended location, then hit enter. Enter X to scroll up or Z to scroll down")
+    #TODO: read inputs, adjust a and b to show different values, and finally return LCODE of selected location
+
+
+
+
+
+    return 1
